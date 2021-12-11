5b7385abe9a2f33bb51baa5bf46ce2b31f442c56
Gopal Ji Gupta <92217615+guptagopalji@users.noreply.github.com>
2021-12-12 02:48:22 +0530

    done
    new file

---
+++ pyspark-collect.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+author SparkByExamples.com
+"""
+
+import pyspark
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('gopal').getOrCreate()
+
+sub = [("Gopal","math",90), \
+    ("Rajesh","science",80), \
+    ("Gopal","hindi",95), \
+    ("Mayank","english",89) \
+  ]
+subColumns = ["name","sub_name","number"]
+subDF = spark.createDataFrame(data=sub, schema = subColumns)
+subDF.printSchema()
+subDF.show(truncate=False)
+
+dataCollect = subDF.collect()
+
+print(dataCollect)
+
+dataCollect2 = subDF.select("sub_name").collect()
+print(dataCollect2)
+
+for row in dataCollect:
+    print(row['name'] + "," + row['sub_name'] + "," +str(row['number']))
